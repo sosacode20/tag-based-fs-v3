501a201bbdeaa97bac1ebc0f025951d37f8212e2
Leismael Sosa <leismael.sosa17@gmail.com>
2025-01-25 23:31:42 -0500

    Adding functionality for obtaining chunks from files and for creating files that store the progress of a file download

---
+++ src/file_helpers/file_download_helper.py
@@ -0,0 +1,215 @@
+import aiofiles
+from pathlib import Path
+from file_helper import calculate_checksum, FileMetadata
+import os
+from pydantic import BaseModel
+from typing import Self
+import asyncio
+import hashlib
+
+# The information to store in the metadata of the download helper are:
+# - total_file_size => 8 bytes
+# - downloaded_size => 8 bytes
+# - file_version    => 4 bytes
+# - file_checksum   => 32 bytes
+
+
+class DownloadMetadata(BaseModel):
+    file_size: int
+    """The total expected size in bytes of the downloaded file"""
+    downloaded_bytes: int
+    """The number of downloaded bytes from the file"""
+    file_version: int
+    """The file version"""
+    file_checksum: int
+    """The expected checksum of the file when download is complete"""
+
+    def to_bytes(self):
+        total_size = self.file_size.to_bytes(length=8)
+        downloaded_size = self.downloaded_bytes.to_bytes(length=8)
+        file_version = self.file_version.to_bytes(length=4)
+        file_checksum = self.file_checksum.to_bytes(length=32)
+        return total_size + downloaded_size + file_version + file_checksum
+
+    @classmethod
+    def from_bytes(cls, metadata_in_bytes: bytes) -> Self:
+        if len(metadata_in_bytes) != 52:
+            raise Exception(
+                f"Invalid metadata bytes. The number of bytes of metadata MUST be 52 bytes"
+            )
+        total_size = int.from_bytes(metadata_in_bytes[:8])
+        downloaded_size = int.from_bytes(metadata_in_bytes[8:16])
+        file_version = int.from_bytes(metadata_in_bytes[16:20])
+        file_checksum = int.from_bytes(metadata_in_bytes[20:52])
+        return DownloadMetadata(
+            file_size=total_size,
+            downloaded_bytes=downloaded_size,
+            file_version=file_version,
+            file_checksum=file_checksum,
+        )
+
+    def to_json(self):
+        """Returns a JSON representation of this metadata"""
+        return self.model_dump_json()
+
+    @classmethod
+    def from_json(cls, json: bytes | str) -> Self:
+        """Parse a Json representation of the `DownloadMetadata`"""
+        return cls.model_validate_json(json_data=json)
+
+    @classmethod
+    def from_file_metadata(cls, file_metadata: FileMetadata) -> Self:
+        """Creates a new `DownloadMetadata` object from a `FileMetadata. Like if it where a new download"""
+        return DownloadMetadata(
+            file_size=file_metadata.size,
+            downloaded_bytes=0,
+            file_version=file_metadata.version,
+            file_checksum=file_metadata.checksum,
+        )
+
+
+class FileDownloadHelper:
+    def __init__(
+        self,
+        file_name: str,
+        storage_directory: Path,
+        download_metadata: DownloadMetadata,
+        overwrite_if_incompatible: bool = False,
+    ):
+        self.file_name: str = file_name
+        """The original name of the file"""
+        self.file_path: Path = storage_directory / f"{file_name}.part"
+        """The path to the partial download of the file"""
+        self.metadata: DownloadMetadata = download_metadata
+        """The metadata of this file download"""
+        self.overwrite_if_incompatible: bool = overwrite_if_incompatible
+        # if not self.file_path.exists():
+        #     metadata_in_file = await FileDownloadHelper.read_metadata(self.file_path)
+
+    async def initialize(self):
+        """Creates the .part file if not exists"""
+        if self.file_path.exists():
+            return
+        self.file_path.touch()
+        await self.append_metadata()
+
+    @staticmethod
+    def get_metadata_size() -> int:
+        """Return the fixed size in bytes of the metadata"""
+        # file_size + downloaded_bytes + file_version + file_checksum = 52 bytes
+        return 8 + 8 + 4 + 32
+
+    @staticmethod
+    async def _read_metadata(file_path: Path) -> DownloadMetadata:
+        """Read the metadata of the file"""
+        if not file_path.exists():
+            raise FileNotFoundError()
+        file_size = file_path.stat().st_size
+        metadata_size = FileDownloadHelper.get_metadata_size()
+        if file_size < metadata_size:
+            raise Exception(
+                f"The file at '{file_path}' is not a valid partial downloaded file"
+            )
+        async with aiofiles.open(file_path, mode="rb") as file:
+            await file.seek(-metadata_size, os.SEEK_END)
+            metadata = await file.read(metadata_size)
+            metadata = DownloadMetadata.from_bytes(metadata_in_bytes=metadata)
+            return metadata
+
+    async def read_metadata(self) -> DownloadMetadata:
+        self.metadata = await FileDownloadHelper._read_metadata(self.file_path)
+        return self.metadata
+
+    async def remove_metadata(self):
+        """Removes the metadata from the partial download file"""
+        if not self.file_path.exists():
+            raise FileNotFoundError()
+        file_size = self.file_path.stat().st_size
+        metadata_size: int = FileDownloadHelper.get_metadata_size()
+        if file_size < metadata_size:
+            raise Exception(
+                f"The file at {self.file_path} has no metadata. It is not a valid partial downloaded file"
+            )
+        # with self.file_path.open(mode="wb") as file:
+        #     # file.seek(-metadata_size, os.SEEK_END)
+        #     await asyncio.to_thread(file.truncate, file_size - metadata_size)
+        new_size: int = file_size - metadata_size
+        async with aiofiles.open(self.file_path, mode="r+b") as file:
+            # file.seek(0, os.SEEK_END)
+            await file.truncate(new_size)
+
+    async def append_metadata(self):
+        """Append the metadata at the end of the file"""
+        if not self.file_path.exists():
+            raise FileNotFoundError()
+        async with aiofiles.open(self.file_path, mode="+ab") as file:
+            await file.write(self.metadata.to_bytes())
+
+    async def get_number_bytes_to_download(self) -> int:
+        """
+        This method returns the number of bytes needed to complete the download
+        This method expects that the file is created and with
+        an associated metadata"""
+        self.metadata = await self.read_metadata()
+        return self.metadata.file_size - self.metadata.downloaded_bytes
+
+    async def write_chunk(self, chunk: bytes):
+        if not self.file_path.exists():
+            raise FileNotFoundError()
+        bytes_to_complete = await self.get_number_bytes_to_download()
+        if len(chunk) > bytes_to_complete:
+            raise Exception(
+                f"The file {self.file_name} only need {bytes_to_complete} bytes to complete the download, but you tried to write {len(chunk)} bytes"
+            )
+        await self.remove_metadata()
+        # metadata = self.metadata
+        async with aiofiles.open(self.file_path, mode="ab") as file:
+            # await file.seek(self.metadata.downloaded_bytes, os.SEEK_SET)  #
+            await file.write(chunk)
+        self.metadata.downloaded_bytes += len(chunk)
+        await self.append_metadata()
+        # await file.write(chunk + metadata.to_bytes())
+        # await file.truncate()
+
+    async def is_download_complete(self) -> bool:
+        """Says if the download is complete"""
+        self.metadata = await self.read_metadata()
+        return self.metadata.downloaded_bytes == self.metadata.file_size
+
+    async def calculate_checksum(self, has_metadata: bool = True) -> int:
+        """Calculate the checksum of the downloaded file"""
+        metadata_size: int = self.get_metadata_size()
+        file_size = self.file_path.stat().st_size
+        if has_metadata:
+            file_size -= metadata_size
+        read_bytes = 0
+        hash_function = hashlib.sha256()
+        async with aiofiles.open(self.file_path, mode="rb+") as file:
+            to_read = min(4096, file_size - read_bytes)
+            while to_read:
+                hash_function.update(await file.read(to_read))
+                read_bytes += to_read
+                to_read = min(4096, file_size - read_bytes)
+        return int.from_bytes(hash_function.digest())
+
+    async def is_download_correct(self) -> bool:
+        """Returns True if the download is complete and if the content has the correcto checksum"""
+        if not await self.is_download_complete():
+            return False
+        actual_checksum = await self.calculate_checksum(has_metadata=True)
+        expected_checksum = self.metadata.file_checksum
+        return actual_checksum == expected_checksum
+
+    async def save_completed_file(self, destination_folder: Path) -> bool:
+        """Save the downloaded file if its completed, to the destination_folder"""
+        # if not await self.is_download_correct():
+        #     return False
+        if not destination_folder.is_dir():
+            raise Exception(
+                f"The destination_folder '{destination_folder}' NEEDS to be a folder"
+            )
+        await self.remove_metadata()
+        await asyncio.threads.to_thread(
+            self.file_path.replace, destination_folder / self.file_name
+        )
+        return True
---
+++ src/file_helpers/file_helper.py
@@ -0,0 +1,348 @@
+from pathlib import Path
+from pydantic import BaseModel, Field
+from datetime import datetime, timezone
+import math
+from enum import Enum
+import hashlib
+from typing import Self
+import aiofiles
+
+
+class SizeUnit(Enum):
+    """The possible file size units"""
+
+    Bytes = 0
+    KB = 1
+    MB = 2
+    GB = 3
+    TB = 4
+    PB = 5
+
+
+def calculate_checksum(file_path: Path) -> int:
+    """
+    Calculate the checksum of a file using the sha256 algorithm.
+
+    Args:
+        file_path (Path): The path to the file for which to calculate the checksum.
+
+    Returns:
+        int: The checksum value as an integer.
+
+    Example:
+    >>> checksum = calculate_checksum(Path("path/to/your/file.txt"))
+    >>> print(checksum)
+    """
+    hash_function = hashlib.sha256()
+    with file_path.open("rb") as f:
+        while chunk := f.read(4096):
+            hash_function.update(chunk)
+    return int.from_bytes(hash_function.digest(), byteorder="big")
+
+
+class FileMetadata(BaseModel):
+    """Metadata of the file"""
+
+    name: str = Field(min_length=1, max_length=255)
+    """The name of the file"""
+    size: int = Field(ge=0, lt=1024**4)
+    """The file size in bytes"""
+    version: int = Field(ge=0, default=0)
+    """The version of the file"""
+    checksum: int = Field(ge=0)
+    """The checksum of the file. It is calculated using the sha256 algorithm"""
+
+    def get_formatted_size(self) -> str:
+        """Get the size and the size unit"""
+        size = abs(self.size)
+        unit_index = min(math.floor(math.log(size, 1024)), 5)
+        new_size = size / 1024**unit_index
+        return f"{new_size:.2f} {SizeUnit(unit_index).name}"
+
+    def get_size(self, unit: SizeUnit = SizeUnit.Bytes) -> str:
+        """Get the size in the specified unit"""
+        return f"{self.size / 1024**int(unit):.2f} {str(unit)}"
+
+    def to_json(self):
+        """Convert the metadata to a JSON string"""
+        return self.model_dump_json()
+
+    @classmethod
+    def from_json(cls, json_str: str | bytes) -> Self:
+        """Create an instance of the class from a JSON string"""
+        return cls.model_validate_json(json_str)
+
+
+class FileHelper(object):
+    """This class is a helper for obtaining chunks of a file. Also, for obtaining metadata of the same"""
+
+    def __init__(self, file_path: Path):
+        self.file_path: Path = file_path
+        """The path of the file"""
+
+    def get_file_size(self) -> int:
+        """Returns the file size in bytes"""
+        return self.file_path.stat().st_size
+
+    def get_metadata(self) -> FileMetadata:
+        """Returns the file metadata."""
+        stats = self.file_path.stat()
+        metadata = FileMetadata(
+            name=self.file_path.name,
+            size=stats.st_size,
+            checksum=calculate_checksum(self.file_path),
+        )
+        return metadata
+
+    def valid_chunk_request(self, offset: int, chunk_size: int) -> bool:
+        """
+        Validates if the requested chunk is within the bounds of the file.
+
+        This method checks if the requested chunk starting at the given 'offset' and of size 'chunk_size'
+        is within the bounds of the file. If the offset exceeds the file size, the method returns False.
+
+        Args:
+            offset (int): The starting position in the file from which to read the chunk (in bytes).
+            chunk_size (int): The size of the chunk to be read (in bytes).
+
+        Returns:
+            bool: True if the requested chunk is within the bounds of the file, False otherwise.
+
+        Example:
+        >>> helper = FileHelper('path/to/your/file.txt')
+        >>> # Check if a chunk of size 1024 bytes starting at the byte offset 32 is valid
+        >>> is_valid = helper.valid_chunk_request(32, 1024)
+        >>> print(is_valid)
+        """
+        size: int = self.get_file_size()
+        return offset < size and offset + chunk_size <= size
+
+    def get_chunk(self, offset: int, chunk_size: int) -> bytes:
+        """
+        Retrieves a chunk of data from the file starting at the specified offset.
+
+        This method reads a single chunk of data of size 'chunk_size' from the file, beginning
+        at the given 'offset'. If the offset exceeds the file size, an exception is raised.
+        The method ensures that the file is opened in binary read mode.
+
+        Args:
+            offset (int): The starting position in the file from which to read the chunk (in bytes).
+            chunk_size (int): The size of the chunk to be read (in bytes).
+
+        Returns:
+            bytes: The data of the specified chunk.
+
+        Raises:
+            Exception: If the specified offset exceeds the total size of the file.
+
+        Example:
+        >>> helper = FileHelper('path/to/your/file.txt')
+        >>> # Give me a chunk of at most 1024 bytes in size starting at the byte offset 32
+        >>> # An error will be raised if the offset is greater than the file size
+        >>> chunk = helper.get_chunk(32, 1024)
+        >>> print(chunk)
+        """
+        size: int = self.get_file_size()
+        if offset > size:
+            raise Exception(
+                f"The file '{self.file_path}' has {size} bytes and the bytes offset is {offset} which is greater than the size of the file."
+            )
+        with self.file_path.open("rb") as file:
+            file.seek(
+                offset
+            )  # if you want to read from the end, then `file.seek(-offset, os.SEEK_END)`
+            fragment = file.read(chunk_size)
+        return fragment
+
+    async def get_chunk_async(self, offset: int, chunk_size: int) -> bytes:
+        """
+        Retrieves a chunk of data from the file starting at the specified offset asynchronously.
+
+        This method reads a single chunk of data of size 'chunk_size' from the file, beginning
+        at the given 'offset'. If the offset exceeds the file size, an exception is raised.
+        The method ensures that the file is opened in binary read mode.
+
+        Args:
+            offset (int): The starting position in the file from which to read the chunk (in bytes).
+            chunk_size (int): The size of the chunk to be read (in bytes).
+
+        Returns:
+            bytes: The data of the specified chunk.
+
+        Raises:
+            Exception: If the specified offset exceeds the total size of the file.
+
+        Example:
+        >>> helper = FileHelper('path/to/your/file.txt')
+        >>> # Give me a chunk of at most 1024 bytes in size starting at the byte offset 32
+        >>> # An error will be raised if the offset is greater than the file size
+        >>> chunk = await helper.get_chunk_async(32, 1024)
+        >>> print(chunk)
+        """
+        size: int = self.get_file_size()
+        if offset > size:
+            raise Exception(
+                f"The file '{self.file_path}' has {size} bytes and the bytes offset is {offset} which is greater than the size of the file."
+            )
+        async with aiofiles.open(self.file_path, "rb") as file:
+            await file.seek(offset)
+            fragment = await file.read(chunk_size)
+        return fragment
+
+    def get_chunks(self, offset: int, chunk_size: int, n_chunks: int) -> list[bytes]:
+        """
+        Retrieves a specified number of chunks of data from the file, starting from a given offset.
+
+        This method reads a maximum of 'n_chunks' from the file, each of size 'chunk_size',
+        beginning at the specified 'offset'. If the offset is beyond the file size, an exception
+        is raised. The method ensures that it does not attempt to read beyond the end of the file.
+
+        Args:
+            offset (int): The starting position in the file from which to read chunks (in bytes).
+            chunk_size (int): The size of each chunk to be read (in bytes).
+            n_chunks (int): The maximum number of chunks to retrieve.
+
+        Returns:
+            list[bytes]: A list containing the retrieved chunks of data,
+                         which may contain fewer than 'n_chunks' if the end of the file is reached.
+
+        Raises:
+            Exception: If the specified offset exceeds the total size of the file.
+
+        Example:
+        >>> helper = FileHelper('path/to/your/file.txt')
+        >>> # Get 5 chunks of size 1024 bytes starting at the byte offset 32
+        >>> # An error will be raised if the offset (32 in this example) is greater than the total size of the file
+        >>> chunks = helper.get_chunks(32, 1024, 5)
+        >>> for i, chunk in enumerate(chunks):
+        >>>     print(f"Chunk {i}: {chunk}")
+        """
+        size: int = self.get_file_size()
+        if offset > size:
+            raise Exception(
+                f"The file '{self.file_path}' has {size} bytes and the bytes offset is {offset} which is greater than the size of the file."
+            )
+        with self.file_path.open("rb") as file:
+            file.seek(offset)
+            fragment = file.read(n_chunks * chunk_size)
+            fragments = []
+            for i in range(n_chunks):
+                if offset + i * chunk_size > size:
+                    break
+                fragments.append(fragment[i * chunk_size : (i + 1) * chunk_size])
+        return fragments
+
+    async def get_chunks_async(
+        self, offset: int, chunk_size: int, n_chunks: int
+    ) -> list[bytes]:
+        """
+        Retrieves a specified number of chunks of data from the file, starting from a given offset asynchronously.
+
+        This method reads a maximum of 'n_chunks' from the file, each of size 'chunk_size',
+        beginning at the specified 'offset'. If the offset is beyond the file size, an exception
+        is raised. The method ensures that it does not attempt to read beyond the end of the file.
+
+        Args:
+            offset (int): The starting position in the file from which to read chunks (in bytes).
+            chunk_size (int): The size of each chunk to be read (in bytes).
+            n_chunks (int): The maximum number of chunks to retrieve.
+
+        Returns:
+            list[bytes]: A list containing the retrieved chunks of data,
+                         which may contain fewer than 'n_chunks' if the end of the file is reached.
+
+        Raises:
+            Exception: If the specified offset exceeds the total size of the file.
+
+        Example:
+        >>> helper = FileHelper('path/to/your/file.txt')
+        >>> # Get 5 chunks of size 1024 bytes starting at the byte offset 32
+        >>> # An error will be raised if the offset (32 in this example) is greater than the total size of the file
+        >>> chunks = await helper.get_chunks_async(32, 1024, 5)
+        >>> for i, chunk in enumerate(chunks):
+        >>>     print(f"Chunk {i}: {chunk}")
+        """
+        size: int = self.get_file_size()
+        if offset > size:
+            raise Exception(
+                f"The file '{self.file_path}' has {size} bytes and the bytes offset is {offset} which is greater than the size of the file."
+            )
+        async with aiofiles.open(self.file_path, "rb") as file:
+            await file.seek(offset)
+            fragment = await file.read(n_chunks * chunk_size)
+            fragments = []
+            for i in range(n_chunks):
+                if offset + i * chunk_size > size:
+                    break
+                fragments.append(fragment[i * chunk_size : (i + 1) * chunk_size])
+        return fragments
+
+    def get_index_chunk(self, chunk_size: int, chunk_index: int) -> bytes:
+        """
+        Retrieves a specific chunk of data from the file based on the provided chunk size and index.
+
+        This method calculates the total number of chunks in the file by dividing the file size by the
+        specified chunk size. It raises an exception if the requested chunk index is out of bounds.
+        If valid, it seeks to the appropriate position in the file and reads the specified chunk of data.
+
+        Args:
+            chunk_size (int): The size of each chunk in bytes.
+            chunk_index (int): The index of the chunk to retrieve (0-based).
+
+        Returns:
+            bytes: The data of the specified chunk.
+
+        Raises:
+            Exception: If the requested chunk index exceeds the total number of available chunks.
+
+        Example:
+        >>> helper = FileHelper('path/to/your/file.txt')
+        >>> # Give me the 3rd chunk of size 1024 bytes from the file. If the file has less than 3 chunks of size 1024, then an error will be raised
+        >>> # NOTE: This method gives you a chunk of 'chunk_size' bytes if you are NOT asking for the last chunk. The last chunk could have less than 'chunk_size' bytes
+        >>> chunk = helper.get_index_chunk(1024, 3)
+        >>> print(chunk)
+        """
+        with self.file_path.open("rb") as file:
+            size: int = self.get_file_size()
+            total_chunks = math.ceil(size / chunk_size)
+            if chunk_index >= total_chunks:
+                raise Exception(
+                    f"The file '{self.file_path.name}' only has {total_chunks} of at most {chunk_size} bytes. You are asking for the {chunk_index} chunk which doesn't exist"
+                )
+            file.seek(chunk_size * chunk_index)
+            return file.read(chunk_size)
+
+    async def get_index_chunk_async(self, chunk_size: int, chunk_index: int) -> bytes:
+        """
+        Retrieves a specific chunk of data from the file based on the provided chunk size and index asynchronously.
+
+        This method calculates the total number of chunks in the file by dividing the file size by the
+        specified chunk size. It raises an exception if the requested chunk index is out of bounds.
+        If valid, it seeks to the appropriate position in the file and reads the specified chunk of data.
+
+        Args:
+            chunk_size (int): The size of each chunk in bytes.
+            chunk_index (int): The index of the chunk to retrieve (0-based).
+
+        Returns:
+            bytes: The data of the specified chunk.
+
+        Raises:
+            Exception: If the requested chunk index exceeds the total number of available chunks.
+
+        Example:
+        >>> helper = FileHelper('path/to/your/file.txt')
+        >>> # Give me the 3rd chunk of size 1024 bytes from the file. If the file has less than 3 chunks of size 1024, then an error will be raised
+        >>> # NOTE: This method gives you a chunk of 'chunk_size' bytes if you are NOT asking for the last chunk. The last chunk could have less than 'chunk_size' bytes
+        >>> chunk = await helper.get_index_chunk_async(1024, 3)
+        >>> print(chunk)
+        """
+        async with aiofiles.open(self.file_path, "rb") as file:
+            size: int = self.get_file_size()
+            total_chunks = math.ceil(size / chunk_size)
+            if chunk_index >= total_chunks:
+                raise Exception(
+                    f"The file '{self.file_path.name}' only has {total_chunks} of at most {chunk_size} bytes. You are asking for the {chunk_index} chunk which doesn't exist"
+                )
+            await file.seek(chunk_size * chunk_index)
+            return await file.read(chunk_size)
